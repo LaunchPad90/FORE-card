cedff3bfc9c6a6bffb124e0122273dca12df5c1a
Jacob Hickey <launchpad@Jacobs-MacBook-Pro.local>
2020-04-11 09:45:13 -0600

    login, signup links

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
 import SignupForm from './components/SignupForm/SignupForm';
+import userService from './utils/userService';
+import NavBar from './components/Navbar/NavBar';
+import { Route, Switch } from 'react-router-dom';
+
 
 class App extends Component {
+  state = {
+    user: userService.getUser() 
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Working</h1>
         <SignupForm />
+        <NavBar />
       </div>
     );
   }
