c96190a53e87aae8ea644cc9727b0b90da95160d
Jacob Hickey <launchpad@Jacobs-MacBook-Pro.local>
2020-04-12 21:45:09 -0600

    step 12!

--- backend/routes/api/scorecard.js
+++ backend/routes/api/scorecard.js
@@ -3,11 +3,14 @@ const router = express.Router();
 const scoreCardCtrl = require('../../controllers/scoreCards');
 
 router.use(require('../../../backend/config/auth'));
-router.get('/', scoreCardCtrl.create);
+router.get('/', checkAuth, scoreCardCtrl.create);
 
 
 
-
+function checkAuth(req, res, next) {
+    if (req.user) return next();
+    return res.status(401).json({msg: 'Not Authorized'});
+  }
 
 
 module.exports = router;
