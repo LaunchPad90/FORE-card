ec2859af32cd908050ef45493c34a895f618c2ed
Jacob Hickey <launchpad@Jacobs-MacBook-Pro.local>
2020-04-16 16:05:55 -0600

    pars populating from course pars array

--- src/components/CardRow/CardRow.jsx
+++ src/components/CardRow/CardRow.jsx
@@ -10,8 +10,7 @@ import Paper from '@material-ui/core/Paper';
 export default function CardRow(props) {
 
     const [cardRow, setCardRow] = useState({
-        holeNum: [1, 2, 3, 4, 5, 6, 6, 8, 9],
-        pars: []
+        holeNum: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18],
 
     })
 
@@ -34,37 +33,45 @@ export default function CardRow(props) {
 
     const classes = useStyles();
 
+    const pars = props.course.pars.map((par, idx) => {
+        return (
+    
+            <Grid container className={classes.container} spacing={1}>
+                <Grid item xs={1}></Grid>
+                {/* for spacing purposes */}
+                    <Grid item xs={2}>
+                        <Paper className={classes.paper}>
+                            <p>{cardRow.holeNum[idx]}</p>
+                        </Paper>
+                    </Grid>
+    
+                    <Grid item xs={2}>
+                        <Paper className={classes.paper}>
+                            <p>{props.course.pars[idx]}</p>
+                        </Paper>
+                    </Grid>
+    
+                    <Grid container item className={classes.container} xs={6}>
+                        <Grid item xs={6}>                    
+                            <Paper className={classes.paper}>
+                                <PlayerScore/>
+                            </Paper>
+                        </Grid>
+                        <Grid item xs={6}>                    
+                            <Paper className={classes.paper}>
+                                <PlayerScore/>
+                            </Paper>
+                        </Grid>
+                    </Grid>
+                {/* for spacing purposes */}
+                <Grid xs={1}></Grid>
+            </Grid>
+        )
+    })
+
     return (
-
-        <Grid container className={classes.container} spacing={1}>
-            <Grid item xs={1}></Grid>
-            {/* for spacing purposes */}
-                <Grid item xs={2}>
-                    <Paper className={classes.paper}>
-                        <p>{cardRow.holeNum[0]}</p>
-                    </Paper>
-                </Grid>
-
-                <Grid item xs={2}>
-                    <Paper className={classes.paper}>
-                        <p>{props.course.pars[0]}</p>
-                    </Paper>
-                </Grid>
-
-                <Grid container item className={classes.container} xs={6}>
-                    <Grid item xs={6}>                    
-                        <Paper className={classes.paper}>
-                            <PlayerScore/>
-                        </Paper>
-                    </Grid>
-                    <Grid item xs={6}>                    
-                        <Paper className={classes.paper}>
-                            <PlayerScore/>
-                        </Paper>
-                    </Grid>
-                </Grid>
-            {/* for spacing purposes */}
-            <Grid xs={1}></Grid>
-        </Grid>
+        <div>
+            {pars}
+        </div>
     )
 }
