9ea41c42ae871db60379981bb9b2d9ec90e2f064
Jacob Hickey <launchpad@Jacobs-MacBook-Pro.local>
2020-04-16 19:22:21 -0600

    Deploy

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 
 const db = mongoose.connection;
 
 db.once('connected', () => {
-  console.log(`Connected to MongoDB ${db.name} at ${db.host}:${db.port}`);
+  console.log(`Connected to MongoDB ${process.env.DATABASE_URL}`);
 });
