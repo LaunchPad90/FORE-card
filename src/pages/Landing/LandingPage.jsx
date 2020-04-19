7d5ab339ba5586d3aef47760fde66ac05637dc7e
Jacob Hickey <launchpad@Jacobs-MacBook-Pro.local>
2020-04-19 16:35:20 -0600

    background image for landing page

--- src/pages/Landing/LandingPage.jsx
+++ src/pages/Landing/LandingPage.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import '../Landing/Landing.css'
+
+
+
+var sectionStyle = {
+    backgroundPosition: 'center',
+    height: "600px",
+    marginTop: "0",
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundImage: "url(" + "/sawgrass-e1552519748368.jpg" + ")"
+  };
+
 
 export default function LandingPage() {
     return (
-        <div>
-            <h1>Welcome to FORE!-card</h1>
-        </div>
+        
+            <section style={ sectionStyle }>
+                <h1>Welcome to FORE!-card</h1>
+            </section>
+        
     )
 }
