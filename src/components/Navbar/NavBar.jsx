fe03882646a4f6011b6724600c91ff5b378b9341
Jacob Hickey <launchpad@Jacobs-MacBook-Pro.local>
2020-05-13 16:11:40 -0600

    attempt to update style

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -1,60 +1,39 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import '../Navbar/NavBar.css';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import red from '@material-ui/core/colors/red';
-import { Button } from '@material-ui/core';
+import styled from "styled-components"
 
-const primary = red[500];
+const NavLinks = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  margin: .5rem;
+  font-size: 1.2rem;
+`;
 
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-    
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
+const NavLink = styled(Link)`
+  text-decoration: none;
+  color: green;
+`;
 
 
 const NavBar = (props) => {
-
-  const classes = useStyles();
   
     let nav = props.user ?
     <div className="nav-container">
-      <AppBar position="static">
-        <Toolbar>
-          <Button>
-            <Link className={classes.menuButton} to='/home' className='nav-link'>{props.user.name}</Link>
-          </Button>
-          <Button>
-            <Link to='/allCourses' className='nav-link'>Courses</Link>
-          </Button>
-          <Button>
-            <Link to='/map' className='nav-link'>Map</Link>
-          </Button>
-          <Button>
-            <Link to='' className='nav-link' onClick={props.handleLogOut}>Logout</Link>
-          </Button>
-        </Toolbar>
-      </AppBar>
+      <NavLinks className="nav-links">
+        <NavLink to="/home" className="nav-links__link">{props.user.name}</NavLink>
+        <NavLink to="/allCourses" className="nav-links__link">Courses</NavLink>
+        <NavLink to="/map" className="nav-links__link">Map</NavLink>
+        <NavLink to="" onClick={props.handleLogOut} className="nav-links__link">Logout</NavLink>
+      </NavLinks>
       </div>
       :
       <div className="nav-container">
-        <AppBar>
-          <Toolbar>
-            <Link to='/login' className='nav-link'>LOG IN</Link>
-            <Link to='/signup' className='nav-link'>SIGN UP</Link>
-          </Toolbar>
-        </AppBar>
+        <NavLinks className="nav-links">
+          <NavLink to="/login" className="nav-links__link 1">LOG IN</NavLink>
+          <NavLink to="/signup" className="nav-links__link 2">SIGN UP</NavLink>
+        </NavLinks>
       </div>;
   
     return (
--- src/pages/Landing/LandingPage.jsx
+++ src/pages/Landing/LandingPage.jsx
@@ -1,24 +1,23 @@
 import React from 'react'
+import styled from 'styled-components'
 import '../Landing/Landing.css'
 
 
 
-var sectionStyle = {
-    backgroundPosition: 'center',
-    height: "600px",
-    marginTop: "0",
-    backgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat',
-    backgroundImage: "url(" + "/sawgrass-e1552519748368.jpg" + ")"
-  };
+const Section = styled.section`
+    backgroundPosition: center;
+    height: 100vh;
+    background: url(https://images.unsplash.com/photo-1589399517461-e1950832ad70?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80);
+    background-size: cover;
+  `;
 
 
 export default function LandingPage() {
     return (
         
-            <section style={ sectionStyle }>
+            <Section>
                 <h1>Welcome to FORE!-card</h1>
-            </section>
+            </Section>
         
     )
 }
