70e8f097b83de20fc357a422f713e4d94c2fef11
Jacob Hickey <launchpad@Jacobs-MacBook-Pro.local>
2020-05-13 16:21:17 -0600

    so much to fix!

--- src/pages/Landing/LandingPage.jsx
+++ src/pages/Landing/LandingPage.jsx
@@ -1,23 +1,20 @@
 import React from 'react'
 import styled from 'styled-components'
 import '../Landing/Landing.css'
-
-
-
-const Section = styled.section`
-    backgroundPosition: center;
-    height: 100vh;
-    background: url(https://images.unsplash.com/photo-1589399517461-e1950832ad70?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80);
+    
+const Page = styled.div`
+    height: 95vh;
+    width: 100vw;
+    background: url(https://images.unsplash.com/photo-1546085879-f751cef230ad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60);
     background-size: cover;
-  `;
-
+`;
 
 export default function LandingPage() {
-    return (
-        
-            <Section>
+    return ( 
+        <Page>   
+            <section>
                 <h1>Welcome to FORE!-card</h1>
-            </Section>
-        
+            </section>
+        </Page>
     )
 }
